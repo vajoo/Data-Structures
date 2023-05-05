bfd61df20ccc2f6de62e2caa9dd2cc0260758b20
vajoo <laurin.windhaus@gmail.com>
2023-05-05 19:08:26 +0200

    added arrays.py file and implemented lists

---
+++ arrays.py
@@ -0,0 +1,13 @@
+new_list = [1, 2, 3]
+result = new_list[0]
+
+if 1 in new_list: print("True")
+
+for n in new_list:
+    if n == 1:
+        print("True")
+        break
+
+# Adding elements to the list
+new_list.append(4)
+print(new_list)
